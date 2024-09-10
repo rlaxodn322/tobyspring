4fd8a756fc6d3755ff33fcfc02c3c54c0949cbcc
taewoo <rlaxodn322@gmail.com>
2024-09-10 17:27:37 +0900

    url get / json 형태 다가져오기

--- src/main/java/tobyspring/PaymentService.java
+++ src/main/java/tobyspring/PaymentService.java
@@ -1,20 +1,37 @@
 package tobyspring;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId,
     String currency,
-    BigDecimal foreignCurrencyAmount){
+    BigDecimal foreignCurrencyAmount) throws IOException {
         // 환율 가져오기
+
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection =(HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+        System.out.println(response);
+        // https://open.er-api.com/v6/latest/USD
         // 금액 계산
         // 유효 시간 계산
         return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.2));
         System.out.println(payment);

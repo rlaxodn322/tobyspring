1eaa19ce3093f4298206eb2a78391d31e607d5bf
taewoo <rlaxodn322@gmail.com>
2024-09-11 15:27:19 +0900

    Client 분리

---
+++ src/main/java/tobyspring/Client.java
@@ -0,0 +1,13 @@
+package tobyspring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new WebApiExRatePaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(51.2));
+        System.out.println(payment);
+    }
+}
+
--- src/main/java/tobyspring/PaymentService.java
+++ src/main/java/tobyspring/PaymentService.java
@@ -14,12 +14,10 @@ import java.net.URLConnection;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
-public class PaymentService {
+abstract public class PaymentService {
     public Payment prepare(Long orderId,
-    String currency,
-    BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
-        // https://open.er-api.com/v6/latest/USD
+                           String currency,
+                           BigDecimal foreignCurrencyAmount) throws IOException {
         BigDecimal exRate = getExRate(currency);
         // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
@@ -29,21 +27,6 @@ public class PaymentService {
 
     }
 
-    private  BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection =(HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        return exRate;
-    }
+    abstract BigDecimal getExRate(String currency) throws IOException;
 
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(51.2));
-        System.out.println(payment);
-    }
 }

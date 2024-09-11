3cd5dfbb001f1b33485b196c7d1390e5d82c3edc
taewoo <rlaxodn322@gmail.com>
2024-09-11 17:16:03 +0900

    class 분리

--- src/main/java/tobyspring/PaymentService.java
+++ src/main/java/tobyspring/PaymentService.java
@@ -1,24 +1,15 @@
 package tobyspring;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
+//Crtl+ ALT + O import 정리
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-abstract public class PaymentService {
+ public class PaymentService {
     public Payment prepare(Long orderId,
                            String currency,
                            BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+        WebApiExRateProvider exRateProvider = new WebApiExRateProvider();
+        BigDecimal exRate = exRateProvider.getWebExRate(currency);
         // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         // 유효 시간 계산
@@ -27,6 +18,5 @@ abstract public class PaymentService {
 
     }
 
-    abstract BigDecimal getExRate(String currency) throws IOException;
 
 }

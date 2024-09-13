a43baa3052ee45ccb04d53c961fe02f202c7a4e8
taewoo <rlaxodn322@gmail.com>
2024-09-13 09:46:31 +0900

    전략 패턴

--- src/main/java/tobyspring/Sort.java
+++ src/main/java/tobyspring/Sort.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 public class Sort {
     public static void main(String[] args) {
-        List<Integer> scores = Arrays.asList(1,5,2,7,3);
-        Collections.sort(scores);
-        scores.forEach(System.out::println);
+//        List<Integer> scores = Arrays.asList(1,5,2,7,3);
+//        Collections.sort(scores);
+//        scores.forEach(System.out::println);
+        List<String> scores2 = Arrays.asList("z", "spring","Java");
+        Collections.sort(scores2, (o1, o2) -> o1.length() - o2.length()  );
+        scores2.forEach(System.out::println);
     }
 }

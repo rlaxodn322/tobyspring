13a27e49275b9b4339ac5bf8e682f996fcab435e
taewoo <rlaxodn322@gmail.com>
2024-10-10 17:17:51 +0900

    paymentService ==

--- src/main/java/tobyspring/Client.java
+++ src/main/java/tobyspring/Client.java
@@ -10,7 +10,10 @@ public class Client {
     public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
-
+        PaymentService paymentService1 = beanFactory.getBean(PaymentService.class);
+        System.out.println(paymentService1);
+        System.out.println(paymentService);
+        System.out.println(paymentService1==paymentService);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(51.2));
 
         System.out.println(payment);

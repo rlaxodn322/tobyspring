be5c12ba20df63d1095d3c98a08f037087747cd8
taewoo <rlaxodn322@gmail.com>
2024-10-16 10:07:18 +0900

    test

--- src/test/java/tobyspring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/payment/PaymentServiceSpringTest.java
@@ -1,8 +1,12 @@
 package tobyspring.payment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.ObjectFactory;
 import tobyspring.TestObjectFactory;
 
@@ -12,13 +16,15 @@ import java.math.BigDecimal;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(SpringExtension.class) // 둘이 한쌍
+@ContextConfiguration(classes = TestObjectFactory.class) // 둘이 한쌍
 class PaymentServiceSpringTest {
-
+    //@Autowired BeanFactory beanFactory;
+    @Autowired PaymentService paymentService;
     @Test
     //@DisplayName("잘 충족")
     void convertedAmount() throws IOException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+        //PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(1L, "KRW", BigDecimal.TEN);
         assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
         assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(10_000));

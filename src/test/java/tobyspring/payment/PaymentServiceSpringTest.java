911445ad81e812d50f2263103049e360e0cca799
taewoo <rlaxodn322@gmail.com>
2024-10-17 08:31:53 +0900

    Clock Test 중간 커밋

--- src/test/java/tobyspring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/payment/PaymentServiceSpringTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tobyspring.ObjectFactory;
 import tobyspring.TestPaymentConfig;
 
 import java.io.IOException;

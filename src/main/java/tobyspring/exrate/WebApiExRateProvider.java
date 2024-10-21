cde0397abbc38dd484f783104ee732614ed4b837
taewoo <rlaxodn322@gmail.com>
2024-10-21 17:41:56 +0900

    ApiTemplate 분리

--- src/main/java/tobyspring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/exrate/WebApiExRateProvider.java
@@ -1,50 +1,22 @@
 package tobyspring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tobyspring.api.ApiExecutor;
-import tobyspring.api.ExApiExRateExtractor;
-import tobyspring.api.ExRateExtractor;
-import tobyspring.api.SimpleApiExecutor;
+import tobyspring.api.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-public class WebApiExRateProvider implements ExRateProvider{
+public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
-    public BigDecimal getExRate(String currency)  {
+    public BigDecimal getExRate(String currency) {
+
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url, new SimpleApiExecutor(), new ExApiExRateExtractor());
+        return apiTemplate.getExRate(url, new HttpClientApiExercutor(), new ExApiExRateExtractor());
         //System.out.println("API Exrate: " + data.rates().get("KRW"));
     }
-
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        String response;
-        try{
-            response = apiExecutor.execute(uri);
-        }
-           catch(IOException e){
-               throw new RuntimeException(e);
-           }
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
 
 }

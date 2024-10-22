ee179050ee07195053aedbb821ff4f2fa00906a2
taewoo <rlaxodn322@gmail.com>
2024-10-22 16:56:14 +0900

    DataAccessException 과 예외 추상화

--- src/main/java/tobyspring/DataConfig.java
+++ src/main/java/tobyspring/DataConfig.java
@@ -1,11 +1,14 @@
 package tobyspring;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import tobyspring.data.OrderRepository;
@@ -30,8 +33,16 @@ public class DataConfig {
         return emf;
     }
     @Bean
-    public OrderRepository orderRepository(EntityManagerFactory emf) {
-        return new OrderRepository(emf);
+    public BeanPostProcessor persistenceAnnotationBeanPostProcessor(){
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
+    @Bean
+    public OrderRepository orderRepository() {
+        return new OrderRepository();
     }
 
 }
